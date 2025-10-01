45a68ddcc9396498f7058d7e1c64a1f46623c443
Daniil0116 <159323772+Daniil0116@users.noreply.github.com>
2025-10-01 18:18:03 +0300

    Add files via upload

---
+++ analyst/docs/competitors/c++/Reverse.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream> 
+#include <string> 
+#include <algorithm> 
+int main() { 
+	std::string input; 
+	std::cout << "Enter a string: "; 
+	std::getline(std::cin, input); 
+	std::string reversed = input; 
+	std::reverse(reversed.begin(), reversed.end()); 
+	std::cout << "Reversed string: " << reversed << std::endl; 
+	return 0; 
+}
---
+++ analyst/docs/competitors/c++/SquareRoot.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream> 
+#include <cmath> 
+#include <iomanip> 
+int main() { 
+	double inputNumber;
+	std::cout << "Enter a real number: "; 
+	std::cin >> inputNumber;
+	if (inputNumber < 0.0)
+	{
+		std::cout << "ERROR" << std::endl; 
+	} 
+	else 
+	{ 
+		double root = std::sqrt(inputNumber);
+		std::cout << "Square root: " << std::fixed << std::setprecision(2) << root << std::endl; 
+	} 
+    return 0; 
+}
